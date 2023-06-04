7b2fa16dfe8b3bf9a5b9b6903053883d87221f03
Mario Areias <mario.s.s.areias@gmail.com>
2023-06-04 19:25:39 +1000

    Making gd to center

--- init.lua
+++ init.lua
@@ -361,6 +361,15 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
+-- Save the current handler: which is used by vim.lsp.buf.definition() function
+local old_handler = vim.lsp.handlers["textDocument/definition"]
+
+-- Override the current handler by adding a call to `zz` to center the screen
+vim.lsp.handlers["textDocument/definition"] = function(err, method, result, client_id, bufnr, config)
+  old_handler(err, method, result, client_id, bufnr, config)
+  vim.schedule(function() vim.cmd('normal! zz') end)
+end
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
