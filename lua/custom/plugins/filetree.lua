25b9a12dc52b0ee61da87650edc961372a01e07a
Mario Areias <mario.s.s.areias@gmail.com>
2023-07-07 17:00:32 +1000

    Autoexpand path when changing current buffer

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -10,6 +10,10 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		require('neo-tree').setup {}
+		require('neo-tree').setup {
+			filesystem = {
+				follow_current_file = true,
+			}
+		}
 	end,
 }
