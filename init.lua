d35bc47ed02f3fff995edd731933e9d1500f09fe
Mario Areias <mario.s.s.areias@gmail.com>
2024-04-07 23:43:33 +1000

    added kdl

--- init.lua
+++ init.lua
@@ -316,7 +316,8 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'gomod', 'gosum', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim',
+  ensure_installed = { 'c', 'cpp', 'go', 'gomod', 'gosum', 'kdl', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc',
+    'vim',
     'terraform' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
@@ -461,7 +462,6 @@ local servers = {
   jsonls = {},
   yamlls = {},
   pylsp = {},
-
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
